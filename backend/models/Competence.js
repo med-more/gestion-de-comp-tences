516e745e09c3ba24918bf94d2f7bc360da30b10c
Mohammed Baba <mbfmz20@gmail.com>
2025-07-01 10:56:31 +0100

    Modèles

---
+++ backend/models/Competence.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const SousCompetenceSchema = new mongoose.Schema({
+  nom: { type: String, required: true },
+  valide: { type: Boolean, default: false },
+});
+
+const CompetenceSchema = new mongoose.Schema(
+  {
+    code: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^C[1-8]$/, 'Le code doit être entre C1 et C8'],
+    },
+    nom: { type: String, required: true },
+    sousCompetences: [SousCompetenceSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Competence', CompetenceSchema);
